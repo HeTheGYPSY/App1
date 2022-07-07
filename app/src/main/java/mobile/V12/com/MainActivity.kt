dda447e5518d6b883aeb496b9f3c4b3211ca6a19
Vernon Hezron <vernonhezron@gmail.com>
2022-07-07 19:30:36 +0300

    First Commit 3

--- app/src/main/java/mobile/V12/com/MainActivity.kt
+++ app/src/main/java/mobile/V12/com/MainActivity.kt
@@ -3,17 +3,10 @@ package mobile.V12.com
 import android.R
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.activity_main, menu)
-        return true
     }
 }
