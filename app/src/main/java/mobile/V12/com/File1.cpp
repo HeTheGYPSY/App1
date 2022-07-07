0bf6e66fc01e070396bdf4c4c7b4dcd760284267
Vernon Hezron <vernonhezron@gmail.com>
2022-07-07 23:20:57 +0300

    First Commit 5

--- app/src/main/java/mobile/V12/com/File1.cpp
+++ app/src/main/java/mobile/V12/com/File1.cpp
@@ -2,3 +2,13 @@
 // Created by User on 7/6/2022.
 //
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "What do you want to say? " << "\n";
+    cin >> resp;
+    cout << "Okay then, we'll " << resp;
+    return 0;
+}
